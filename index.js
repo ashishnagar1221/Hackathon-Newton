0bf4bb85a48c967370437b9f25eeef47a7f78a0a
Ashish <ashishnagar1221@gmail.com>
2020-03-29 21:49:50 +0530

    Hello World!

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+
+const port = process.env.PORT || 3600;
+
+var app = express();
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+app.listen(port , function(){
+    console.log(`Listening at port ${port}`);
+})
