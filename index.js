5da71bf08dc8d507110d158f935b3c573a1e677c
Ashish <ashishnagar1221@gmail.com>
2020-03-29 22:21:29 +0530

    'render'

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const routes = require('./routes/birth')
 
 const port = process.env.PORT || 3600;
 
 var app = express();
 
-app.get('/', (req, res)=>{
-   res.send("Hello world!");
-});
+app.use('/',routes);
 
 app.listen(port , function(){
     console.log(`Listening at port ${port}`);
--- routes/birth.js
+++ routes/birth.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
 
+
+router.get('/', (req, res)=>{
+    res.Render('index');
+ });
+
+
 module.exports = router;
