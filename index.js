42f9bca455c383eb9ad59af1df54b21b57f36412
Ashish <ashishnagar1221@gmail.com>
2020-03-29 22:54:23 +0530

    mongo URI

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const routes = require('./routes/birth')
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3600;
+
+mongoose.connect('mongodb+srv://user:user@ashish-gupgf.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true,useUnifiedTopology: true })
+.then(()=> console.log('Conected to Database...'))
+.catch(err =>console.log(err));
 
 var app = express();
 
