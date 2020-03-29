260e3d0010ebbdde71d367da36193f274c6cab28
Ashish <ashishnagar1221@gmail.com>
2020-03-29 22:26:40 +0530

    ejs added

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3600;
 
 var app = express();
 
+app.set('view engine','ejs')
+app.use(express.static(__dirname + "/public"));
 app.use('/',routes);
 
 app.listen(port , function(){
